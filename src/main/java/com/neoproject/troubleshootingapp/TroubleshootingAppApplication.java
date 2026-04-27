b18d0ee137079a382f53aa293e2444d35ad3f411
iljaKorn <kornilov.ilja@rambler.ru>
2026-04-27 20:43:47 +0300

    Initial commit

---
+++ src/main/java/com/neoproject/troubleshootingapp/TroubleshootingAppApplication.java
@@ -0,0 +1,13 @@
+package com.neoproject.troubleshootingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleshootingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TroubleshootingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neoproject/troubleshootingapp/TroubleshootingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoproject.troubleshootingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleshootingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
